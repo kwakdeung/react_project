5f4a8f32b040d3d880b7199c2e18c34e0f9556c9
kwakdeung <97221228+kwakdeung@users.noreply.github.com>
2024-06-18 15:03:28 +0900

    2-4 context api로 사용자 정보 관리하기

--- fastcampus-blog-app/src/index.tsx
+++ fastcampus-blog-app/src/index.tsx
@@ -5,6 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
 import firebase from "firebaseApp";
+import { AuthContextProvider } from "context/AuthContext";
 
 console.log(firebase);
 
@@ -12,11 +13,11 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <AuthContextProvider>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>
+  </AuthContextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
