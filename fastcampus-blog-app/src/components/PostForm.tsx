d5f5f63789f3056207f98963bb7f32bc865775ff
kwakdeung <97221228+kwakdeung@users.noreply.github.com>
2024-06-19 17:24:50 +0900

    2-6 Firestore 쿼리 적용하기 카테고리 탭 구현

--- fastcampus-blog-app/src/components/PostForm.tsx
+++ fastcampus-blog-app/src/components/PostForm.tsx
@@ -4,7 +4,7 @@ import { db } from "firebaseApp";
 import AuthContext from "context/AuthContext";
 import { useNavigate, useParams } from "react-router-dom";
 import { toast } from "react-toastify";
-import { PostProps } from "./PostList";
+import { CATEGORIES, CategoryType, PostProps } from "./PostList";
 
 export default function PostForm() {
   const params = useParams();
@@ -12,6 +12,7 @@ export default function PostForm() {
   const [title, setTitle] = useState<string>("");
   const [summary, setSummary] = useState<string>("");
   const [content, setContent] = useState<string>("");
+  const [category, setCategory] = useState<CategoryType>("Frontend");
   const { user } = useContext(AuthContext);
   const navigate = useNavigate();
   console.log(post);
@@ -32,6 +33,7 @@ export default function PostForm() {
             minute: "2-digit",
             second: "2-digit",
           }),
+          category: category,
         });
         toast?.success("게시글을 수정했습니다.");
         navigate(`/posts/${post.id}`);
@@ -48,6 +50,7 @@ export default function PostForm() {
           }),
           email: user?.email,
           uid: user?.uid,
+          category: category,
         });
 
         toast?.success("게시글을 생성했습니다.");
@@ -60,7 +63,9 @@ export default function PostForm() {
   };
 
   const onChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >
   ) => {
     const {
       target: { name, value },
@@ -76,6 +81,10 @@ export default function PostForm() {
 
     if (name === "content") {
       setContent(value);
+    }
+
+    if (name === "category") {
+      setCategory(value as CategoryType);
     }
   };
 
@@ -97,6 +106,7 @@ export default function PostForm() {
       setTitle(post?.title);
       setSummary(post?.summary);
       setContent(post?.content);
+      setCategory(post?.category as CategoryType);
     }
   }, [post]);
 
@@ -112,6 +122,22 @@ export default function PostForm() {
           onChange={onChange}
           value={title}
         />
+      </div>
+      <div className="form__block">
+        <label htmlFor="category">카테고리</label>
+        <select
+          name="category"
+          id="category"
+          onChange={onChange}
+          defaultValue={category}
+        >
+          <option value="">카테고리를 선택해주세요</option>
+          {CATEGORIES?.map((category) => (
+            <option value={category} key={category}>
+              {category}
+            </option>
+          ))}
+        </select>
       </div>
       <div className="form__block">
         <label htmlFor="summary">요약</label>
--- fastcampus-blog-app/src/components/PostList.tsx
+++ fastcampus-blog-app/src/components/PostList.tsx
@@ -15,10 +15,8 @@ import { toast } from "react-toastify";
 
 interface PostListProps {
   hasNavigation?: boolean;
-  defaultTap?: TabType;
+  defaultTap?: TabType | CategoryType;
 }
-
-type TabType = "all" | "my";
 
 export interface PostProps {
   id?: string;
@@ -29,13 +27,24 @@ export interface PostProps {
   createdAt: string;
   updatedAt?: string;
   uid: string;
+  category?: CategoryType;
 }
+
+type TabType = "all" | "my";
+
+export type CategoryType = "Frontend" | "Backend" | "Web" | "Native";
+export const CATEGORIES: CategoryType[] = [
+  "Frontend",
+  "Backend",
+  "Web",
+  "Native",
+];
 
 export default function PostList({
   hasNavigation = true,
   defaultTap = "all",
 }: PostListProps) {
-  const [activeTab, setActiveTab] = useState<TabType>("all");
+  const [activeTab, setActiveTab] = useState<TabType | CategoryType>("all");
   const [posts, setPosts] = useState<PostProps[]>([]);
   const { user } = useContext(AuthContext);
 
@@ -46,9 +55,16 @@ export default function PostList({
     if (activeTab === "my" && user) {
       // 나의 글만 필터링
       postsQuery = query(postsRef, where("uid", "==", user.uid));
-    } else {
+    } else if (activeTab === "all") {
       // 모든 글 보여주기
       postsQuery = query(postsRef, orderBy("createdAt", "asc"));
+    } else {
+      // 카테고리 글 보여주기
+      postsQuery = query(
+        postsRef,
+        where("category", "==", activeTab),
+        orderBy("createdAt", "asc")
+      );
     }
     const datas = await getDocs(postsQuery);
     datas?.forEach((doc) => {
@@ -90,6 +106,18 @@ export default function PostList({
           >
             나의 글
           </div>
+          {CATEGORIES?.map((category) => (
+            <div
+              key={category}
+              role="presentation"
+              onClick={() => setActiveTab(category)}
+              className={
+                activeTab === category ? "post__navigation--active" : ""
+              }
+            >
+              {category}
+            </div>
+          ))}
         </div>
       )}
       <div className="post__list">
