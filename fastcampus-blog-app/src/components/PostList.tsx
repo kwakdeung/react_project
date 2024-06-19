b8841219ad1845bb550f2887bd70cd91f05683c3
kwakdeung <97221228+kwakdeung@users.noreply.github.com>
2024-06-19 10:44:57 +0900

    2-6 Firestore 쿼리 적용하기 내가 쓴 글 탭 구현

--- fastcampus-blog-app/src/components/PostList.tsx
+++ fastcampus-blog-app/src/components/PostList.tsx
@@ -1,5 +1,13 @@
 import AuthContext from "context/AuthContext";
-import { collection, deleteDoc, doc, getDocs } from "firebase/firestore";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
 import { db } from "firebaseApp";
 import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
@@ -7,6 +15,7 @@ import { toast } from "react-toastify";
 
 interface PostListProps {
   hasNavigation?: boolean;
+  defaultTap?: TabType;
 }
 
 type TabType = "all" | "my";
@@ -22,14 +31,26 @@ export interface PostProps {
   uid: string;
 }
 
-export default function PostList({ hasNavigation = true }: PostListProps) {
+export default function PostList({
+  hasNavigation = true,
+  defaultTap = "all",
+}: PostListProps) {
   const [activeTab, setActiveTab] = useState<TabType>("all");
   const [posts, setPosts] = useState<PostProps[]>([]);
   const { user } = useContext(AuthContext);
 
   const getPosts = async () => {
-    const datas = await getDocs(collection(db, "posts"));
     setPosts([]);
+    let postsRef = collection(db, "posts");
+    let postsQuery;
+    if (activeTab === "my" && user) {
+      // 나의 글만 필터링
+      postsQuery = query(postsRef, where("uid", "==", user.uid));
+    } else {
+      // 모든 글 보여주기
+      postsQuery = query(postsRef, orderBy("createdAt", "asc"));
+    }
+    const datas = await getDocs(postsQuery);
     datas?.forEach((doc) => {
       console.log(doc.data(), doc.id);
       const dataObj = { ...doc.data(), id: doc.id };
@@ -49,7 +70,7 @@ export default function PostList({ hasNavigation = true }: PostListProps) {
 
   useEffect(() => {
     getPosts();
-  }, []);
+  }, [activeTab]);
 
   return (
     <>
@@ -73,7 +94,7 @@ export default function PostList({ hasNavigation = true }: PostListProps) {
       )}
       <div className="post__list">
         {posts?.length > 0 ? (
-          posts?.map((post, index) => (
+          posts?.map((post) => (
             <div key={post?.id} className="post__box">
               <Link to={`/posts/${post?.id}`}>
                 <div className="post__profile-box">
