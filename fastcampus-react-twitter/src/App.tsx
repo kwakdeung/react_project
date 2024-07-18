98fba169e51d05ba83effd205cb673b8e42a9661
kwakdeung <rhkrwnsdud22@naver.com>
2024-07-18 10:17:08 +0900

    3-3 Firebase Auth로 기본 회원가입 실습 1

--- fastcampus-react-twitter/src/App.tsx
+++ fastcampus-react-twitter/src/App.tsx
@@ -1,20 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Router from "components/Router";
 import { Layout } from "components/Layout";
 
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { app } from "firebaseApp";
+import { ToastContainer } from "react-toastify";
+
+import Loader from "components/loader/Loader";
 
 function App() {
   const auth = getAuth(app);
+  const [init, setInit] = useState<boolean>(false);
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
     !!auth?.currentUser
   );
 
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+      setInit(true);
+    });
+  }, [auth]);
+
   return (
     <Layout>
-      <Router />
+      <ToastContainer />
+      {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
     </Layout>
   );
 }
